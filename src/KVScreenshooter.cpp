8bc6d4f0d8d2bd2559c5d9b44b7375d7444e25d7
Yukariin <yukariin@yandex.ru>
2014-07-30 00:35:00 +0400

    Remove unecessary conversion

--- src/KVScreenshooter.cpp
+++ src/KVScreenshooter.cpp
@@ -25,7 +25,6 @@ KVScreenshooter::KVScreenshooter(QObject *parent) :
 	QObject(parent)
 {
 	manager = new QNetworkAccessManager(this);
-	//connect(manager, SIGNAL(finished(QNetworkReply*)), this, SLOT(uploadingFinished(QNetworkReply*)));
 }
 
 KVScreenshooter::~KVScreenshooter()
@@ -76,7 +75,7 @@ void KVScreenshooter::uploadScreenshot(QImage image)
 
 	QNetworkRequest request(QUrl("https://api.imgur.com/3/image.json"));
 	request.setHeader(QNetworkRequest::ContentTypeHeader, "application/x-www-form-urlencoded");
-	request.setRawHeader(QByteArray("Authorization"), QByteArray("Client-ID ef6bd901726e8b7"));
+	request.setRawHeader("Authorization", "Client-ID ef6bd901726e8b7");
 
 	QByteArray requestBody;
 	requestBody.append(QString("image=").toUtf8());
