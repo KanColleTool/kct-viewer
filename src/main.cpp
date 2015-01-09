8f031cc19a84207232aca48cd4436c8108d6d708
Yukarin <yukariin@yandex.ru>
2015-01-09 15:35:41 +0300

    Remove usless stuff

--- src/main.cpp
+++ src/main.cpp
@@ -7,20 +7,17 @@
 
 int main(int argc, char *argv[])
 {
-	QApplication app(argc, argv);
+	QApplication a(argc, argv);
 	QCoreApplication::setApplicationName("KanColleTool");
 	QCoreApplication::setApplicationVersion(KCT_VERSION);
 	QCoreApplication::setOrganizationName("KanColleTool");
 
 	QWebSettings::globalSettings()->setAttribute(QWebSettings::PluginsEnabled, true);
 
-	QStringList args = QCoreApplication::arguments();
+	//QStringList args = QCoreApplication::arguments();
 
-	KVMainWindow *win = new KVMainWindow();
-	win->show();
+	KVMainWindow w;
+	w.show();
 
-	int ret = app.exec();
-	delete win;
-
-	return ret;
+	return a.exec();
 }
