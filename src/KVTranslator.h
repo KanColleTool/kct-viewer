36e5efba24983dee6f1a3c6200d68faa94973ad4
uppfinnarn <uppfinnarn@gmail.com>
2015-08-06 03:20:10 +0200

    Comments

--- src/KVTranslator.h
+++ src/KVTranslator.h
@@ -5,6 +5,9 @@
 #include <QString>
 #include <QHash>
 
+/**
+ * Translator for in-game strings.
+ */
 class KVTranslator : public QObject
 {
 	Q_OBJECT
@@ -52,19 +55,27 @@ public:
 	 * Translates a Japanese phrase into the loaded language.
 	 * 
 	 * Returns the original string if no translation is loaded, if there's no
-	 * match for the string's CRC32 sum, or if the result is a NULL variant.
+	 * match for the string's CRC32 sum, or if the string is untranslatable.
+	 * 
+	 * @see isTranslatable()
 	 */
 	QString translate(const QString &phrase) const;
 	
 	/**
 	 * Is the given phrase even translatable?
 	 * 
-	 * Examples of untranslatable strings are empty and numeric ones. This is
-	 * checked by translate() before it attempts a translation lookup.
+	 * Examples of untranslatable strings are empty and numeric ones.
 	 */
 	bool isTranslatable(const QString &phrase) const;
 	
 protected:
+	/**
+	 * Dictionary of registered translations.
+	 * 
+	 * Keys are CRC32 checksums of the original strings, values are translated
+	 * counterparts in the current language. The translator does not know or
+	 * care what it's translating to, as long as it's representable in UTF-8.
+	 */
 	QHash<quint32, QString> dictionary;
 };
 
