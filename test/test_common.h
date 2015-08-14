1141af1d2b5126fb98d642ada7f34ab15c826ec0
uppfinnarn <uppfinnarn@gmail.com>
2015-08-14 11:26:41 +0200

    std::cout << QUrl()

--- test/test_common.h
+++ test/test_common.h
@@ -1,5 +1,6 @@
 #include <catch.hpp>
 #include <QString>
+#include <QUrl>
 
 /// Allow printing of QStrings to std::cout
 inline std::ostream& operator<<(std::ostream& os, const QString& string) {
@@ -9,4 +10,9 @@ inline std::ostream& operator<<(std::ostream& os, const QString& string) {
 // Allow printing of QByteArrays to std::cout
 inline std::ostream& operator<<(std::ostream& os, const QByteArray& data) {
 	return os << data.toStdString();
+}
+
+// Allow printing of QUrls to std::cout
+inline std::ostream& operator<<(std::ostream& os, const QUrl& url) {
+	return os << url.toString();
 }
